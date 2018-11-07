3789d08bf358d5e654582746baaf3ac0cfe4013e
Jenny Hoelzer <hoelzer.jenny@gmail.com>
2018-11-07 13:24:19 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -19,43 +19,25 @@ const CardGroup = (props) => (
 );
 
 const Card = (props) => (
-    <div>TODO: Implement Card Component</div>
+    <div className="card cardGroup__card">
+        <div className="card__description cardGroup__cardDescription">
+            <div className={`icon fa ${props.icon} card__descriptionIcon`}></div>
+            <div className="card__descriptionText">
+                {props.description}
+                <br/>
+                {props.hint && <span>{props.hint}</span>}
+            </div>
+        </div>
+        <div className="card__price">{props.price}</div>
+    </div>
 );
 
 const App = () => (
-    <div className="cardGroup">
-        <div className="card cardGroup__card">
-            <div className="card__description cardGroup__cardDescription">
-                <div className="icon fa fa-thumbs-o-up card__descriptionIcon" />
-                <div className="card__descriptionText">
-                    Trial
-                </div>
-            </div>
-            <div className="card__price">Free!</div>
-        </div>
-        <div className="card cardGroup__card">
-            <div className="card__description cardGroup__cardDescription">
-                <div className="icon fa fa-trophy card__descriptionIcon" />
-                <div className="card__descriptionText">
-                    Basic tier
-                    <br/>
-                    (most popular)
-                </div>
-            </div>
-            <div className="card__price">$10.00</div>
-        </div>
-        <div className="card cardGroup__card">
-            <div className="card__description cardGroup__cardDescription">
-                <div className="icon fa fa-bolt card__descriptionIcon" />
-                <div className="card__descriptionText">
-                    Advanced tier
-                    <br/>
-                    (only for enterprise-level professionals)
-                </div>
-            </div>
-            <div className="card__price">$6,000.00</div>
-        </div>
-    </div>
+    <CardGroup>
+        <Card description="Trial" price="Free" icon="fa-thumbs-o-up" />
+        <Card description="Basic Tier" price="$10.00" hint="Most Popular" icon="fa-trophy" />
+        <Card description="Advanced Tier" hint="Only for Enterprise-Level Professionals" price="$6,000.00" icon="fa-bolt" />
+    </CardGroup>
 );
 
 export default App;
